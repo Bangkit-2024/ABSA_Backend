e221b6183090eb8f7572ee1bec6f7eb1d2c3ffed
Fernando Nikolas R <nikolasfernando2001@gmail.com>
2024-05-21 12:02:56 +0800

    feat: :sparkles: Add Factory for bulk mock data

---
+++ content/factory.py
@@ -0,0 +1,56 @@
+import factory
+from django.contrib.auth.models import User
+import factory.fuzzy
+from random import choice
+from .models import Company, Review, ReviewAspectSentiment, Aspect
+
+class CompanyFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Company
+    
+    name = factory.Faker("company")
+    location = factory.Faker("address",locale="id_ID")
+    description = factory.Faker("paragraph",nb_sentences=15)
+
+    @factory.post_generation
+    def user_to_company(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for user in extracted:
+                self.user_to_company.add(user)
+ 
+
+class ReviewFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = Review
+
+    review_text = factory.Faker("sentence",nb_words=10)
+
+    @factory.lazy_attribute
+    def company(self):
+        company = Company.objects.all()
+        if Company.objects.exists():
+            return choice(company)
+        else:
+            return CompanyFactory.create()
+
+class ReviewAspectSentimentFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = ReviewAspectSentiment
+    
+    sentiment = factory.fuzzy.FuzzyInteger(-1,1)
+
+    @factory.lazy_attribute
+    def aspect(self):
+        aspects = Aspect.objects.all()
+        if Aspect.objects.exists():
+            return choice(aspects)
+        
+    @factory.lazy_attribute
+    def review(self):
+        aspects = Review.objects.all()
+        if Review.objects.exists():
+            return choice(aspects)
