20fc3b72c0bdcee87aaf47279e8d5ba70ec66015
Fernando Nikolas R <nikolasfernando2001@gmail.com>
2024-05-29 19:14:07 +0800

    fix: :bug: Fix bug in importing

--- absa/settings/staging.py
+++ absa/settings/staging.py
@@ -1,6 +1,17 @@
 from absa.settings.common import *
 from utils.environment import load_env, get_env
+import os
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USERNAME"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST':os.environ.get("DB_HOST"),
+        'PORT':'3306',
+    }
+}
